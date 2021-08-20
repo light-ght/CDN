84af898d6b338a0c1154c7caf223395594474938
light-ght <2799633873@qq.com>
2021-08-20 11:46:29 +0800

    4

--- title.js
+++ title.js
@@ -1,14 +1,14 @@
- var OriginTitle = document.title;
+﻿ var OriginTitle = document.title;
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = '某女星性感泳装照流出，这身材也太Hot了吧！';
+         document.title = '女星性感泳装照流出，这身材也太Hot了吧！';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = '你在想什么？(*▔＾▔*) ' + OriginTitle;
+         document.title = '你在想什么？(*▔＾▔*) ';
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
