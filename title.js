7dd8e1abb38562ea80fa9c4688c59ce6e01ef005
light-ght <2799633873@qq.com>
2021-08-20 15:07:09 +0800

    4

--- title.js
+++ title.js
@@ -9,8 +9,8 @@
      else {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
          document.title = '你在想什么? (*▔＾▔*)';
-         titleTime = setTimeout(function () {
+         titleTime = setTimeout(2000, function () {
              document.title = OriginTitle;
-         }, 2000);
+         });
      }
  });
