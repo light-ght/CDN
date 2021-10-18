ce2a9878b8ab79fd9b769f9e6681b916554c9794
ligt-ght <2799633873@qq.com>
2021-10-18 21:48:16 +0800

    10/18

---
+++ pics1/title.js
@@ -0,0 +1,16 @@
+﻿ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = '我还没玩够，来陪我鸭(,,• ₃ •,,)';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = '你回来辣！ヾ（o◕ˇ∀ˇ◕）ツ ';
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 1500);
+     }
+ });
